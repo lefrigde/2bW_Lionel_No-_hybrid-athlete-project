0db6082b31cea845f8f2d5ad148325dfa491443e
lefrigde <lionel@schnupftabak.ch>
2026-03-06 09:11:13 +0100

    supabase javascript

--- js/script.js
+++ js/script.js
@@ -22,7 +22,35 @@ if (hrForm) {
       result.textContent = "Bitte ein gültiges Alter (10–90) eingeben.";
       return;
     }
+// Supabase Verbindung
+const supabaseUrl = "https://swensdexmakrolenkxcs.supabase.co";
+const supabaseKey = "sb_publishable_pV-jHdq6Qzex6JtKFCAgNQ_O9yA6w58";
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
+const goalForm = document.getElementById("goalForm");
+
+if (goalForm) {
+  goalForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const name = document.getElementById("goalName").value;
+    const goal = document.getElementById("goalType").value;
+    const level = document.getElementById("goalLevel").value;
+    const message = document.getElementById("goalMessage");
+
+    const { error } = await supabase
+      .from("training_goals")
+      .insert([{ name, goal, level }]);
+
+    if (error) {
+      message.textContent = "Fehler beim Speichern.";
+      console.log(error);
+    } else {
+      message.textContent = "Ziel erfolgreich gespeichert.";
+      goalForm.reset();
+    }
+  });
+}
     const maxHr = 220 - age;
     result.textContent = `Geschätzter Maximalpuls: ${maxHr} bpm`;
   });
